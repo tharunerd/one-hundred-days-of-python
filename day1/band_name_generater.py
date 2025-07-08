158b3f78e5ccdf1023d44a179324ba0f32b89881
tharun <208r5a0402@gmail.com>
2025-07-08 09:59:37 +0530

    changes

--- day1/band_name_generater.py
+++ day1/band_name_generater.py
@@ -1,3 +1,12 @@
+# Band Name Generator
+
+# This simple Python program helps you come up with a fun band name!
+# It asks you for the name of the city you grew up in and your pet’s name,
+# then combines them to suggest a unique band name. 
+# It’s a beginner-friendly project to practice taking user input
+#  and displaying output in Python.
+
+
 print("welcome to the band name generator")
 city = input("which city did you grow up in?\n")
 # print(city)
