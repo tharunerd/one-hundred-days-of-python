b921af05665408f27238d81066fef8d9cfc89e4f
tharun <208r5a0402@gmail.com>
2025-07-10 13:04:58 +0530

    upgrading

---
+++ day_08/main.py
@@ -0,0 +1,31 @@
+# Caesar cipher
+
+def caesar(text, shift, direction):
+    result = ""
+    for char in text:
+        if char.isalpha():
+            shift_amount = shift % 26
+            base = ord('a') if char.islower() else ord('A')
+            if direction == "encode":
+                result += chr((ord(char) - base + shift_amount) % 26 + base)
+            elif direction == "decode":
+                result += chr((ord(char) - base - shift_amount) % 26 + base)
+        else:
+            result += char
+    return result
+
+print("88")
+should_continue = True
+
+while should_continue:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    result = caesar(text, shift, direction)
+    print(f"Here's the {direction}d result: {result}")
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n").lower()
+    if restart != "yes":
+        should_continue = False
+        print("Goodbye")
+
+
